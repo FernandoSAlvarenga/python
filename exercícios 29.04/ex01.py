a2be21f7cfea3f9fe9401e9d6900b9a85cefb485
Fernando dos Santos Alvarenga <670600568@senacinformatica.br>
2024-04-29 21:42:17 -0300

    tk

---
+++ exercícios 29.04/ex01.py
@@ -0,0 +1,8 @@
+'''1. Crie uma interface gráfica com um título, um rótulo e um botão.  
+O título da janela deve ser "Minha Primeira Interface". 
+O rótulo deve exibir o texto "Olá, Tkinter!". 
+O botão deve ter o texto "Clique em Mim". '''
+
+import tkinter as tk
+
+#começar literalmente pelo exercício:
