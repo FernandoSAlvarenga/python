add00b5ea2b981ee5cdb25f1477efa3959c889a6
Fernando dos Santos Alvarenga <670600568@senacinformatica.br>
2024-06-24 20:49:18 -0300

    atualização

--- TK/teste2.py
+++ TK/teste2.py
@@ -5,9 +5,9 @@ import datetime
 
 # Conectar ao MySQL
 elo = mysql.connector.connect(
-    host='127.0.0.1',
+    host='localhost',
     user='root',
-    password='',
+    password='',  # Coloque a senha do seu MySQL se configurado
     database='pizzadofe'
 )
 
@@ -23,9 +23,9 @@ if num_results == 0:
 
     # Reconectar especificando o banco de dados recém-criado
     elo = mysql.connector.connect(
-        host='127.0.0.1',
+        host='localhost',
         user='root',
-        password='',
+        password='',  # Coloque a senha do seu MySQL se configurado
         database='pizzadofe'
     )
     cursor = elo.cursor()
@@ -34,6 +34,7 @@ if num_results == 0:
     cursor.execute('''
         CREATE TABLE pedidos (
             id INT AUTO_INCREMENT PRIMARY KEY,
+            numero_pedido INT UNIQUE,
             data DATETIME NOT NULL,
             tamanho VARCHAR(255),
             quantidade INT,
@@ -46,11 +47,13 @@ if num_results == 0:
     cursor.execute('''
         CREATE TABLE clientes (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            id_cliente VARCHAR(10) NOT NULL UNIQUE,
+            numero_cliente INT UNIQUE,
             nome VARCHAR(255) NOT NULL,
-            endereco VARCHAR(255) NOT NULL,
+            sobrenome VARCHAR(255) NOT NULL,
+            data_nascimento DATE NOT NULL,
             telefone VARCHAR(20) NOT NULL,
-            email VARCHAR(255) NOT NULL
+            email VARCHAR(255) NOT NULL,
+            endereco VARCHAR(255) NOT NULL
         )
     ''')
 
@@ -61,19 +64,30 @@ def criar_tela_clientes():
     tela_clientes.title("Clientes")
 
     def adicionar_cliente():
-        id_cliente = entrada_id_cliente.get()
-        nome = entrada_nome_cliente.get()
-        endereco = entrada_endereco_cliente.get()
-        telefone = entrada_telefone_cliente.get()
-        email = entrada_email_cliente.get()
+        nome = entrada_nome.get()
+        sobrenome = entrada_sobrenome.get()
+        data_nascimento = entrada_data_nascimento.get()
+        telefone = entrada_telefone.get()
+        email = entrada_email.get()
+        endereco = entrada_endereco.get()
 
-        if not id_cliente or not nome or not endereco or not telefone or not email:
+        if not nome or not sobrenome or not data_nascimento or not telefone or not email or not endereco:
             messagebox.showerror("Erro", "Por favor, preencha todos os campos do cliente.")
             return
 
         try:
-            cursor.execute("INSERT INTO clientes (id_cliente, nome, endereco, telefone, email) VALUES (%s, %s, %s, %s, %s)",
-                           (id_cliente, nome, endereco, telefone, email))
+            # Pegar o próximo número de cliente disponível
+            cursor.execute("SELECT MAX(numero_cliente) FROM clientes")
+            max_numero_cliente = cursor.fetchone()[0]
+            if max_numero_cliente is None:
+                max_numero_cliente = 0
+            numero_cliente = max_numero_cliente + 1
+
+            # Mostra o número do cliente ao usuário
+            messagebox.showinfo("Número do Cliente", f"Número do Cliente: {numero_cliente}")
+
+            cursor.execute("INSERT INTO clientes (numero_cliente, nome, sobrenome, data_nascimento, telefone, email, endereco) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                        (numero_cliente, nome, sobrenome, data_nascimento, telefone, email, endereco))
             elo.commit()
             messagebox.showinfo("Cliente Adicionado", "Novo cliente cadastrado com sucesso!")
             tela_clientes.destroy()
@@ -83,194 +97,109 @@ def criar_tela_clientes():
     lbl_titulo_cliente = tk.Label(tela_clientes, text="Novo Cliente", font=("Helvetica", 16))
     lbl_titulo_cliente.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
 
-    lbl_id_cliente = tk.Label(tela_clientes, text="ID do Cliente:")
-    lbl_id_cliente.grid(row=1, column=0, padx=5, pady=5, sticky="w")
-    entrada_id_cliente = tk.Entry(tela_clientes)
-    entrada_id_cliente.grid(row=1, column=1, padx=5, pady=5)
+    lbl_nome = tk.Label(tela_clientes, text="Nome:")
+    lbl_nome.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+    entrada_nome = tk.Entry(tela_clientes)
+    entrada_nome.grid(row=1, column=1, padx=5, pady=5)
 
-    lbl_nome_cliente = tk.Label(tela_clientes, text="Nome:")
-    lbl_nome_cliente.grid(row=2, column=0, padx=5, pady=5, sticky="w")
-    entrada_nome_cliente = tk.Entry(tela_clientes)
-    entrada_nome_cliente.grid(row=2, column=1, padx=5, pady=5)
+    lbl_sobrenome = tk.Label(tela_clientes, text="Sobrenome:")
+    lbl_sobrenome.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+    entrada_sobrenome = tk.Entry(tela_clientes)
+    entrada_sobrenome.grid(row=2, column=1, padx=5, pady=5)
 
-    lbl_endereco_cliente = tk.Label(tela_clientes, text="Endereço:")
-    lbl_endereco_cliente.grid(row=3, column=0, padx=5, pady=5, sticky="w")
-    entrada_endereco_cliente = tk.Entry(tela_clientes)
-    entrada_endereco_cliente.grid(row=3, column=1, padx=5, pady=5)
+    lbl_data_nascimento = tk.Label(tela_clientes, text="Data de Nascimento (AAAA-MM-DD):")
+    lbl_data_nascimento.grid(row=3, column=0, padx=5, pady=5, sticky="w")
+    entrada_data_nascimento = tk.Entry(tela_clientes)
+    entrada_data_nascimento.grid(row=3, column=1, padx=5, pady=5)
 
-    lbl_telefone_cliente = tk.Label(tela_clientes, text="Telefone:")
-    lbl_telefone_cliente.grid(row=4, column=0, padx=5, pady=5, sticky="w")
-    entrada_telefone_cliente = tk.Entry(tela_clientes)
-    entrada_telefone_cliente.grid(row=4, column=1, padx=5, pady=5)
+    lbl_telefone = tk.Label(tela_clientes, text="Telefone:")
+    lbl_telefone.grid(row=4, column=0, padx=5, pady=5, sticky="w")
+    entrada_telefone = tk.Entry(tela_clientes)
+    entrada_telefone.grid(row=4, column=1, padx=5, pady=5)
 
-    lbl_email_cliente = tk.Label(tela_clientes, text="Email:")
-    lbl_email_cliente.grid(row=5, column=0, padx=5, pady=5, sticky="w")
-    entrada_email_cliente = tk.Entry(tela_clientes)
-    entrada_email_cliente.grid(row=5, column=1, padx=5, pady=5)
+    lbl_email = tk.Label(tela_clientes, text="Email:")
+    lbl_email.grid(row=5, column=0, padx=5, pady=5, sticky="w")
+    entrada_email = tk.Entry(tela_clientes)
+    entrada_email.grid(row=5, column=1, padx=5, pady=5)
+
+    lbl_endereco = tk.Label(tela_clientes, text="Endereço:")
+    lbl_endereco.grid(row=6, column=0, padx=5, pady=5, sticky="w")
+    entrada_endereco = tk.Entry(tela_clientes)
+    entrada_endereco.grid(row=6, column=1, padx=5, pady=5)
 
     botao_adicionar_cliente = tk.Button(tela_clientes, text="Adicionar Cliente", command=adicionar_cliente)
-    botao_adicionar_cliente.grid(row=6, column=0, columnspan=2, padx=5, pady=5)
+    botao_adicionar_cliente.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
 
     botao_fechar = tk.Button(tela_clientes, text="Fechar", command=tela_clientes.destroy)
-    botao_fechar.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
+    botao_fechar.grid(row=8, column=0, columnspan=2, padx=5, pady=5)
 
 
 def fazer_pedido():
-    nome = entrada_nome.get()
-    endereco = entrada_endereco.get()
-    telefone = entrada_telefone.get()
-    email = entrada_email.get()
+    tela_pedidos = tk.Toplevel(janela)
+    tela_pedidos.title("Fazer Pedido")
 
-    if not nome or not endereco or not telefone or not email:
-        messagebox.showerror("Erro", "Por favor, preencha todos os campos do cliente.")
-        return
+    lbl_numero_pedido = tk.Label(tela_pedidos, text=f"Número do Pedido: {numero_pedido}", font=("Helvetica", 12))
+    lbl_numero_pedido.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
-    cursor.execute("SELECT id, nome FROM clientes WHERE telefone = %s", (telefone,))
-    cliente = cursor.fetchone()
-    if cliente:
-        cliente_id = cliente[0]
-        cliente_nome = cliente[1]
-    else:
-        if messagebox.askyesno("Cliente Não Encontrado", "Cliente não encontrado. Deseja cadastrar um novo cliente?"):
-            criar_tela_clientes()
+    lbl_numero_cliente = tk.Label(tela_pedidos, text="Número do Cliente:", font=("Helvetica", 12))
+    lbl_numero_cliente.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+
+    entrada_numero_cliente = tk.Entry(tela_pedidos)
+    entrada_numero_cliente.grid(row=1, column=1, padx=5, pady=5)
+
+    def finalizar_pedido():
+        numero_cliente_digitado = entrada_numero_cliente.get()
+
+        try:
+            numero_cliente = int(numero_cliente_digitado)
+            if numero_cliente <= 0:
+                messagebox.showerror("Erro", "Número do cliente deve ser um número inteiro positivo.")
+                return
+        except ValueError:
+            messagebox.showerror("Erro", "Número do cliente deve ser um número inteiro positivo.")
             return
-        else:
-            return
 
-    tamanho = var_tamanho.get()
+        # Aqui você pode implementar a lógica para o pedido (ex: selecionar itens, calcular total, etc.)
+        # Por enquanto, vamos apenas mostrar uma mensagem para ilustrar o funcionamento
 
-    try:
-        quantidade = int(entrada_quantidade.get())
-        if quantidade <= 0:
-            raise ValueError
-    except ValueError:
-        messagebox.showerror("Erro", "Por favor, insira um número válido de pizzas.")
-        return
+        messagebox.showinfo("Pedido Finalizado", f"Pedido para o cliente {numero_cliente} realizado com sucesso!")
+        tela_pedidos.destroy()
 
-    sabores_selecionados = []
-    for i, sabor_var in enumerate(sabores_vars):
-        if sabor_var.get() == 1:
-            sabores_selecionados.append(sabores[i])
+    botao_finalizar_pedido = tk.Button(tela_pedidos, text="Finalizar Pedido", command=finalizar_pedido)
+    botao_finalizar_pedido.grid(row=2, column=1, padx=5, pady=5, sticky="e")
 
-    if tamanho == "Pequena" and len(sabores_selecionados) != 1:
-        messagebox.showerror("Erro", "Pizza pequena só pode ter 1 sabor.")
-        return
-    elif tamanho == "Média" and len(sabores_selecionados) > 2:
-        messagebox.showerror("Erro", "Pizza média só pode ter até 2 sabores.")
-        return
-    elif tamanho == "Grande" and len(sabores_selecionados) > 4:
-        messagebox.showerror("Erro", "Pizza grande só pode ter até 4 sabores.")
-        return
+    botao_cancelar = tk.Button(tela_pedidos, text="Cancelar", command=tela_pedidos.destroy)
+    botao_cancelar.grid(row=2, column=0, padx=5, pady=5, sticky="w")
 
-    ingredientes_selecionados = []
-    for i, ing_var in enumerate(ingredientes_vars):
-        if ing_var.get() == 1:
-            ingredientes_selecionados.append(ingredientes[i])
-
-    total = precos[tamanho] * quantidade
-    total_ingredientes = sum(precos_ingredientes[ing] for ing in ingredientes_selecionados) * quantidade
-    total_pedido = total + total_ingredientes
-    mensagem_sabores = ", ".join(sabores_selecionados)
-    mensagem_ingredientes = ", ".join(ingredientes_selecionados)
-    mensagem_pedido = f"Pedido realizado: {quantidade} pizza(s) {tamanho} com os sabores: {mensagem_sabores}.\n"
-    mensagem_pedido += f"Ingredientes adicionais: {mensagem_ingredientes}.\n"
-    mensagem_pedido += f"Total a pagar: R${(total_pedido):.2f}\n"
-    mensagem_pedido += f"ID do Cliente: {cliente_id}\n"
-    mensagem_pedido += f"Nome do Cliente: {cliente_nome}"
-
-    if messagebox.askyesno("Confirmar Pedido", mensagem_pedido):
-        messagebox.showinfo("Pedido Realizado", "Seu pedido foi confirmado!")
-        cursor.execute("INSERT INTO pedidos (data, tamanho, quantidade, valor_total, cliente_id) VALUES (%s, %s, %s, %s, %s)",
-                       (datetime.datetime.now(), tamanho, quantidade, total_pedido, cliente_id))
-        elo.commit()
+    janela.protocol("WM_DELETE_WINDOW", janela.quit)
 
 
-tamanhos = ["Pequena", "Média", "Grande"]
-precos = {"Pequena": 17.00, "Média": 25.00, "Grande": 32.00}
+def abrir_tela_clientes():
+    criar_tela_clientes()
 
-sabores = [
-    "Frango",
-    "4 Queijos",
-    "Iscas de Carne ao Alho e Óleo",
-    "Brócolis com Queijo Cremoso"
-]
 
-ingredientes = ["Queijo Extra", "Pepperoni", "Bacon"]
-precos_ingredientes = {"Queijo Extra": 6.00, "Pepperoni": 9.00, "Bacon": 7.00}
+def abrir_tela_pedidos():
+    fazer_pedido()
+
 
 janela = tk.Tk()
-janela.title("Pedido de Pizza")
+janela.title("Pizzaria do Fe")
 
-lbl_titulo = tk.Label(janela, text="Dados do Cliente", font=("Helvetica", 16))
-lbl_titulo.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
+# Variáveis para manter números de cliente e pedido visíveis
+numero_cliente = 0
+numero_pedido = 0
 
-lbl_nome = tk.Label(janela, text="Nome:")
-lbl_nome.grid(row=1, column=0, padx=5, pady=5, sticky="w")
-entrada_nome = tk.Entry(janela)
-entrada_nome.grid(row=1, column=1, padx=5, pady=5)
+# Criar um menu inicial
+menu = tk.Menu(janela)
+janela.config(menu=menu)
 
-lbl_endereco = tk.Label(janela, text="Endereço:")
-lbl_endereco.grid(row=2, column=0, padx=5, pady=5, sticky="w")
-entrada_endereco = tk.Entry(janela)
-entrada_endereco.grid(row=2, column=1, padx=5, pady=5)
+menu_clientes = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label="Clientes", menu=menu_clientes)
+menu_clientes.add_command(label="Cadastrar Novo Cliente", command=abrir_tela_clientes)
 
-lbl_telefone = tk.Label(janela, text="Telefone:")
-lbl_telefone.grid(row=3, column=0, padx=5, pady=5, sticky="w")
-entrada_telefone = tk.Entry(janela)
-entrada_telefone.grid(row=3, column=1, padx=5, pady=5)
+menu_pedidos = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label="Pedidos", menu=menu_pedidos)
+menu_pedidos.add_command(label="Fazer Novo Pedido", command=abrir_tela_pedidos)
 
-lbl_email = tk.Label(janela, text="Email:")
-lbl_email.grid(row=4, column=0, padx=5, pady=5, sticky="w")
-entrada_email = tk.Entry(janela)
-entrada_email.grid(row=4, column=1, padx=5, pady=5)
-
-lbl_titulo_pizza = tk.Label(janela, text="Escolha o Tamanho da Pizza", font=("Helvetica", 16))
-lbl_titulo_pizza.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
-
-lbl_precos = tk.Label(janela, text=f"Pequena: R${precos['Pequena']:.2f}\nMédia: R${precos['Média']:.2f}\nGrande: R${precos['Grande']:.2f}")
-lbl_precos.grid(row=6, column=0, columnspan=2, padx=5, pady=5)
-
-lbl_tamanho = tk.Label(janela, text="Tamanho da Pizza:")
-lbl_tamanho.grid(row=7, column=0, padx=5, pady=5)
-var_tamanho = tk.StringVar(janela)
-var_tamanho.set(tamanhos[0])
-menu_tamanho = tk.OptionMenu(janela, var_tamanho, *tamanhos)
-menu_tamanho.grid(row=7, column=1, padx=5, pady=5)
-
-lbl_sabores = tk.Label(janela, text="Sabores:")
-lbl_sabores.grid(row=8, column=0, padx=5, pady=5, sticky="w")
-
-sabores_vars = []
-for i, sabor in enumerate(sabores):
-    var_sabor = tk.IntVar(janela)
-    cb_sabor = tk.Checkbutton(janela, text=sabor, variable=var_sabor)
-    cb_sabor.grid(row=9 + i, column=0, padx=5, pady=2, sticky="w")
-    sabores_vars.append(var_sabor)
-
-lbl_ingredientes = tk.Label(janela, text="Ingredientes Adicionais:")
-lbl_ingredientes.grid(row=9 + len(sabores), column=0, padx=5, pady=5, sticky="w")
-
-ingredientes_vars = []
-for i, ingrediente in enumerate(ingredientes):
-    var_ingrediente = tk.IntVar(janela)
-    cb_ingrediente = tk.Checkbutton(janela, text=ingrediente, variable=var_ingrediente)
-    cb_ingrediente.grid(row=10 + len(sabores) + i, column=0, padx=5, pady=2, sticky="w")
-    ingredientes_vars.append(var_ingrediente)
-
-lbl_quantidade = tk.Label(janela, text="Quantidade de Pizzas:")
-lbl_quantidade.grid(row=10 + len(sabores) + len(ingredientes), column=0, padx=5, pady=5)
-entrada_quantidade = tk.Entry(janela)
-entrada_quantidade.grid(row=10 + len(sabores) + len(ingredientes), column=1, padx=5, pady=5)
-
-botao_pedido = tk.Button(janela, text="Pedir", command=fazer_pedido)
-botao_pedido.grid(row=11 + len(sabores) + len(ingredientes), column=0, columnspan=2, padx=5, pady=5)
-
-# Barra de menu para Clientes e Pedidos
-barra_menu = tk.Menu(janela)
-janela.config(menu=barra_menu)
-
-menu_clientes = tk.Menu(barra_menu, tearoff=0)
-barra_menu.add_cascade(label="Clientes", menu=menu_clientes)
-menu_clientes.add_command(label="Novo Cliente", command=criar_tela_clientes)
-
+# Mostrar a janela principal
 janela.mainloop()
